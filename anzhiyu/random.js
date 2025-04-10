6565bc53057b870424bf52e4db7fc3d889d71cf6
liancheng123 <2500209604@qq.com>
2025-04-11 00:11:57 +0800

    Site updated: 2025-04-11 00:11:56

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/04/07/hello-world/","2025/04/09/平衡车学习day1/"];function toRandomPost(){
+var posts=["2025/04/07/hello-world/","2025/04/09/平衡车学习day1/","2025/04/11/平衡车学习day2/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
